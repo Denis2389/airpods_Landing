ea0db19a6c533726afd098a52ef2f7bec5102fab
Denis2389 <bereznyakdenis2003@gmail.com>
2024-08-08 21:00:43 +0300

    complete

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+document.querySelector('.menu_btn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const targetToBuySection = document.querySelector('.choose_color')
+    targetToBuySection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.querySelector('.menu_info').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const targetToInfoSection = document.querySelector('.advantages')
+    targetToInfoSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.querySelector('.menu_product').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const targetToProductSection = document.querySelector('.composition')
+    targetToProductSection.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
