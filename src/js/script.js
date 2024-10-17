9183d3021969bfe1ea48a6dcff8a327b0de1ce1a
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-17 13:32:53 +0300

    red

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,40 @@ document.querySelector('.menu_product').addEventListener('click', function(event
 });
 
 
+const todoList = document.querySelector('.todo-list');
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+
+
+// todoForm.addEventListener('submit', formHandler)
+
+// function formHandler(event) {
+//     event.preventDefault();
+
+//     const taskText = todoInput.value;
+
+//     const liHTML = `<li>${taskText}</li>`;
+//     todoList.insertAdjacentHTML('beforeend', liHTML);
+
+//     todoInput.value = '';
+//     todoInput.focus();
+// }
+
+    function formHandler(event) {
+        event.preventDefault();
+
+        const taskText = todoInput.value;
+
+        // const liHTML = `<li>${taskText}</li>`
+
+        // todoList.insertAdjacentHTML('beforeend', liHTML)
+
+        const newTask = document.createElement('li')
+        newTask.innerText = taskText;
+        
+        todoList.append(newTask)
+
+        todoInput.value = '';
+
+        todoInput.focus();
+    }
