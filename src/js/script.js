c510cf2bf76f36ee959d8ea7401d4af83148f633
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-17 13:44:44 +0300

    ready

--- src/js/script.js
+++ src/js/script.js
@@ -30,29 +30,10 @@ const todoList = document.querySelector('.todo-list');
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
 
-
-// todoForm.addEventListener('submit', formHandler)
-
-// function formHandler(event) {
-//     event.preventDefault();
-
-//     const taskText = todoInput.value;
-
-//     const liHTML = `<li>${taskText}</li>`;
-//     todoList.insertAdjacentHTML('beforeend', liHTML);
-
-//     todoInput.value = '';
-//     todoInput.focus();
-// }
-
     function formHandler(event) {
         event.preventDefault();
 
         const taskText = todoInput.value;
-
-        // const liHTML = `<li>${taskText}</li>`
-
-        // todoList.insertAdjacentHTML('beforeend', liHTML)
 
         const newTask = document.createElement('li')
         newTask.innerText = taskText;
